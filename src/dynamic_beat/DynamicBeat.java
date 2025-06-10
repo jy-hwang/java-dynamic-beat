29650ab31cbc488551a4bcc30bfb767eb9486e3d
jyhwang <stratojy@gmail.com>
2025-06-11 01:24:21 +0900

    #2. feat - 게임 시작 화면 개발하기
    1. Double Buffer - repaint 를 이용해서 배경 이미지를 넣어줌

--- src/dynamic_beat/DynamicBeat.java
+++ src/dynamic_beat/DynamicBeat.java
@@ -1,8 +1,16 @@
 package dynamic_beat;
 
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class DynamicBeat extends JFrame {
+
+  private Image screenImage;
+  private Graphics screenGraphic;
+
+  private Image introBackground;
 
   public DynamicBeat() {
     setTitle("Dynamic Beat");
@@ -11,5 +19,25 @@ public class DynamicBeat extends JFrame {
     setLocationRelativeTo(null);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setVisible(true);
+
+    ImageIcon backgroundImage = new ImageIcon(Main.class.getResource("/images/introBackground.jpg"));
+    
+    System.out.println(backgroundImage);
+    introBackground = backgroundImage.getImage();
+
   }
+
+  public void paint(Graphics g) {
+    screenImage = createImage(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
+    System.out.println(screenImage);
+    screenGraphic = screenImage.getGraphics();
+    screenDraw(screenGraphic);
+    g.drawImage(screenImage, 0, 0, null);
+  }
+
+  public void screenDraw(Graphics g) {
+    g.drawImage(introBackground, 0, 0, null);
+    this.repaint();
+  }
+
 }
